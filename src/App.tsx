4ea2349f073d6b19e1c87d224b81f3d523f4b56b
Andrew Kachanov <andrew.kachanov@gmail.com>
2017-07-31 18:36:14 +0300

    Step 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import { Button } from 'antd';
 
 const logo = require('./logo.svg');
 
@@ -14,6 +15,7 @@ class App extends React.Component<{}, {}> {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button type="primary">Test</Button>
       </div>
     );
   }
