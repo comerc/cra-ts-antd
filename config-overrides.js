57119a62ba70c7543459e62bdbfde641e99bf5e1
Andrew Kachanov <andrew.kachanov@gmail.com>
2017-07-31 14:22:25 +0300

    Step 3

---
+++ config-overrides.js
@@ -0,0 +1,25 @@
+module.exports = function override(config, env) {
+  const tsLoader = config.module.rules.find(conf => {
+    return conf.loader && conf.loader.includes('ts-loader')
+  })
+  tsLoader.loader = require.resolve('awesome-typescript-loader')
+  tsLoader.query = {
+    useBabel: true,
+  }
+
+  const tsLintLoader = config.module.rules.find(conf => {
+    return conf.loader && conf.loader.includes('tslint-loader')
+  })
+  tsLintLoader.options = tsLintLoader.options || {}
+  // FIXED Warning: The 'no-use-before-declare' rule requires type infomation.
+  tsLintLoader.options.typeCheck = true
+
+  const rewireLess = require('react-app-rewire-less')
+  config = rewireLess(config, env)
+
+  const path = require('path')
+  // For import with absolute path
+  config.resolve.modules = [path.resolve('src')].concat(config.resolve.modules)
+
+  return config
+}
